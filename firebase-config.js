89856b5d06fc074e3797d3aad5fc7610993114cf
edomos <alf.ramoz@gmail.com>
2022-05-21 01:33:15 -0700

    firebase init

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4-uugQHcP06lq_iqdFynrgquwyCfOXjs",
+  authDomain: "twitter-clone-abe6d.firebaseapp.com",
+  projectId: "twitter-clone-abe6d",
+  storageBucket: "twitter-clone-abe6d.appspot.com",
+  messagingSenderId: "677248913601",
+  appId: "1:677248913601:web:9096ff11babde2308fc440"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
