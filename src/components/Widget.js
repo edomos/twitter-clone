be84aa7e399c98dcf92f9c2470e503fe4ccac445
edomos <alf.ramoz@gmail.com>
2022-05-22 01:58:19 -0700

    create styling for widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,10 +1,31 @@
+import { Search } from '@mui/icons-material';
 import React from 'react';
+import { TwitterShareButton, TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
 import '../css/Widget.css'
 
 const Widget = () => {
     return (
-        <div className="widget__container">
-            widget
+        <div className="widget">
+            <div className="widget__input">
+                <Search className="widget__input--searchicon"/>
+                <input type="text" placeholder="Search Twitter" />
+            </div>
+            <div className="widget__container">
+                <h2>What's happening?</h2>
+
+                <TwitterTweetEmbed tweetId={"858551177860055040"} />
+
+                <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="cleverqazi"
+                options={{height: 400}}
+                />
+
+                <TwitterShareButton
+                url={"https://fatcebook.com/cleverprogrammer"}
+                options={{text:"reactjs is awesome", via: "cleverqazi"}}
+                />
+            </div>
         </div>
     );
 }
